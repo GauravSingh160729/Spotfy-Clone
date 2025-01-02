51ac07e1a0cdb3fc0fb10051929eea869b83fba0
GauravSingh160729 <Gauravsingh160729@gmail.com>
2025-01-02 13:07:49 +0530

    do afterzz

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let audioElement = new Audio('gs.mp3');
 // audioElement.play();
 
-let playButton =document.getElementById("#play");
+let playButton =document.getElementById("play");
 
 
-playButton.addEventListener= ('click' ,()=>{
-if(audioElement.paused){
-    audioElement.play();
-}
+
+playButton.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        playButton.classList.toggle('fa-solid fa-play');
+        playButton.classList.toggle('fa-solid fa-circle-pause');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+       playButton.classList.toggle('"fa-solid fa-circle-pause"');
+        playButton.classList.toggle('fa-solid fa-play');
+        gif.style.opacity = 0;
+    }
 })
